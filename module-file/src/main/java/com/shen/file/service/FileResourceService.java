22eb4639145cc4c6a4e6b0805c09d3d64e3021ec
shield <1264932975@qq.com>
2026-07-09 11:59:53 +0800

    文件记录

--- module-file/src/main/java/com/shen/file/service/FileResourceService.java
+++ module-file/src/main/java/com/shen/file/service/FileResourceService.java
@@ -1,13 +1,46 @@
 package com.shen.file.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.shen.file.entity.FileResource;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Date;
+
 /**
-* @author shield
-* @description 针对表【file_resource(文件资源表)】的数据库操作Service
-* @createDate 2026-07-09 11:55:29
-*/
+ * 文件资源 Service
+ */
 public interface FileResourceService extends IService<FileResource> {
 
+    /**
+     * 保存文件资源
+     *
+     * @param path 文件相对路径
+     * @param thumbPath 缩略图相对路径（可为空）
+     * @return 文件资源实体
+     */
+    FileResource saveFileResource(String path, String thumbPath);
+
+    /**
+     * 增加引用计数（索引+1）
+     *
+     * @param id 文件资源ID
+     */
+    void incrementRefCount(Long id);
+
+    /**
+     * 减少引用计数（索引-1）
+     *
+     * @param id 文件资源ID
+     */
+    void decrementRefCount(Long id);
+
+    /**
+     * 分页查询未被引用且超时的记录
+     *
+     * @param currentPage 当前页
+     * @param pageSize 每页大小
+     * @param bufferDate 缓冲时间（创建时间早于此时间的记录）
+     * @return 分页结果
+     */
+    Page<FileResource> pageRefZero(Integer currentPage, Integer pageSize, Date bufferDate);
 }
--- module-file/src/main/java/com/shen/file/service/impl/FileResourceServiceImpl.java
+++ module-file/src/main/java/com/shen/file/service/impl/FileResourceServiceImpl.java
@@ -1,22 +1,54 @@
 package com.shen.file.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shen.file.entity.FileResource;
-import com.shen.file.service.FileResourceService;
 import com.shen.file.mapper.FileResourceMapper;
+import com.shen.file.service.FileResourceService;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 /**
-* @author shield
-* @description 针对表【file_resource(文件资源表)】的数据库操作Service实现
-* @createDate 2026-07-09 11:55:29
-*/
+ * 文件资源 Service 实现
+ */
 @Service
 public class FileResourceServiceImpl extends ServiceImpl<FileResourceMapper, FileResource>
-    implements FileResourceService{
+        implements FileResourceService {
 
+    @Override
+    public FileResource saveFileResource(String path, String thumbPath) {
+        FileResource fileResource = new FileResource();
+        fileResource.setPath(path);
+        fileResource.setThumbPath(thumbPath);
+        fileResource.setRefCount(1);
+        this.save(fileResource);
+        return fileResource;
+    }
+
+    @Override
+    public void incrementRefCount(Long id) {
+        this.lambdaUpdate()
+                .setSql("ref_count = ref_count + 1")
+                .eq(FileResource::getId, id)
+                .update();
+    }
+
+    @Override
+    public void decrementRefCount(Long id) {
+        this.lambdaUpdate()
+                .setSql("ref_count = ref_count - 1")
+                .eq(FileResource::getId, id)
+                .update();
+    }
+
+    @Override
+    public Page<FileResource> pageRefZero(Integer currentPage, Integer pageSize, Date bufferDate) {
+        LambdaQueryWrapper<FileResource> qw = new LambdaQueryWrapper<>();
+        qw.eq(FileResource::getRefCount, 0)
+                .lt(FileResource::getCreateTime, bufferDate);
+        Page<FileResource> page = new Page<>(currentPage, pageSize);
+        return this.page(page, qw);
+    }
 }
-
-
-
-

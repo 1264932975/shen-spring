ca86ca946b668dfe2d1e79bc34bd37d537356e5f
shield <1264932975@qq.com>
2026-07-13 16:41:18 +0800

    启动聚合service和删除无用import

--- module-file/src/main/java/com/shen/file/entity/FileResource.java
+++ module-file/src/main/java/com/shen/file/entity/FileResource.java
@@ -1,8 +1,5 @@
 package com.shen.file.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.shen.framework.base.BaseEntity;
 import lombok.Data;
